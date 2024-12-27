cded6cccb4b719f36076a8c3348fb7e9253f537c
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-12-27 16:38:55 +0530

    done code for displaying x and o on board

--- src/TicTacToe/Sqaure.jsx
+++ src/TicTacToe/Sqaure.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Square = () =>{
+const Square = (props) =>{
     return(
         <>
             <div 
+            onClick={props.onClick}
             style={{border:'1px solid black', height:'100px', width:'100%', display:'flex', justifyContent:'center', alignItems:'center'}}
              className='square'>
-                <h5>X</h5>
+                <h5> {props.value} </h5>
             </div>
         </>
     )
