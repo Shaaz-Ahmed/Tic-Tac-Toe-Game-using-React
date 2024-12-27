e4cea8d6e8764d8e667de286d9e2c1461bf90389
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-12-27 16:12:21 +0530

    tic tac toe code of User interface

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Board from './TicTacToe/Board';
+import Square from './TicTacToe/Sqaure';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = ()=>{
+  return(
+    <>
+      <h3>Tic Tac Toe App</h3>
+      <Board />
+ 
+    </>
+  )
+};
 
 export default App;
